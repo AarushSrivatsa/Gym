de056182c9408f0e13f830ac88375478d1f78338
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-03-07 21:19:58 +0530

    a

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+BREVO_SENDER_EMAIL = "pitlaaarushsrivatsa@gmail.com"
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+
+SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret-key-in-production")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_HOURS = 24
+REFRESH_TOKEN_EXPIRE_DAYS = 30
+
---
+++ backend/main.py
@@ -0,0 +1,44 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+from routers.authentication import router as authentication_router
+from routers.conversations import router as conversation_router
+from routers.messages import router as message_router
+from routers.models import router as model_router
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from contextlib import asynccontextmanager
+from utilities.scheduled_tasks import delete_unnecessary_otps_in_db
+
+scheduler = AsyncIOScheduler()
+router_list = [authentication_router,conversation_router,message_router,model_router]
+task_list = [delete_unnecessary_otps_in_db]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    for task in task_list:
+        scheduler.add_job(task,CronTrigger(hour=0,minute=0,timezone="Asia/Kolkata"))
+    scheduler.start()
+    yield
+    scheduler.shutdown()
+
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+
+app = FastAPI(
+    title="Chatbot Wrapper Backend",
+    description="""
+Built by **Aarush Srivatsa**
+GitHub Docs Link: https://github.com/AarushSrivatsa/Chatbot-Wrapper-Project-Backend-OpenDocs
+Linkedin Profile: https://www.linkedin.com/in/aarushsrivatsa/
+""", 
+    lifespan=lifespan,
+    docs_url="/docs" if DEBUG else None,
+    redoc_url="/redoc" if DEBUG else None,
+    openapi_url="/openapi.json" if DEBUG else None,
+    )
+
+for router in router_list:
+    app.include_router(router)
