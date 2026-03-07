46912a02a6dd27ad5e7bbfcee021acd23277969f
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-03-07 21:26:45 +0530

    deekshith if youre reading this commit then you are gay

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 BREVO_SENDER_EMAIL = "pitlaaarushsrivatsa@gmail.com"
 BREVO_API_KEY = os.getenv("BREVO_API_KEY")
@@ -8,4 +9,3 @@ SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret-key-in-production")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_HOURS = 24
 REFRESH_TOKEN_EXPIRE_DAYS = 30
-
--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 from routers.authentication import router as authentication_router
-from routers.conversations import router as conversation_router
-from routers.messages import router as message_router
-from routers.models import router as model_router
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -14,7 +11,7 @@ from contextlib import asynccontextmanager
 from utilities.scheduled_tasks import delete_unnecessary_otps_in_db
 
 scheduler = AsyncIOScheduler()
-router_list = [authentication_router,conversation_router,message_router,model_router]
+router_list = [authentication_router]
 task_list = [delete_unnecessary_otps_in_db]
 
 @asynccontextmanager
